34e7e08d4fd1b381b76d82602dd79b3572af287b
Marc-Andre Gardner <marc.andre.gardner@gmail.com>
2016-12-18 01:04:28 -0500

    Fix end lines on windows

--- lexparser.py
+++ lexparser.py
@@ -224,7 +224,7 @@ def t_REFLABEL(t):
 
 # Define a rule so we can track line numbers
 def t_newline(t):
-    r'\n+'
+    r'\r*\n'
     t.lexer.lineno += len(t.value)
 
 # A string containing ignored characters (spaces and tabs)
