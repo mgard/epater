55b80e1f651a344bc03bbd4897a18103c73e7485
Yannick Hold <yannickhold@gmail.com>
2017-01-23 17:20:57 -0500

    Reset maximum number of instruction in run to a more believable amount.

--- settings.py
+++ settings.py
@@ -3,7 +3,7 @@ import time
 _settings = {"PCbehavior": "+8",        # Can be "+0", "+8", "real"
              "PCspecialbehavior": True, # True or False, whether we want to turn on or off the +4 for PC
                                         # with special instructions (STR from PC and dataop with PC shifted by register)
-             "runmaxit": 100000,          # Maximum number of non-stop iterations
+             "runmaxit": 1000,          # Maximum number of non-stop iterations
              "maxhistorylength": 1000,  # Maximum history depth
              "fillValue": 0xFF}         # Value used to fill non-initialized (but declared) memory
 
